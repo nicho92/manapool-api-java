aa0fd9c2553117bbc72074dc642a66d29f414669
nicol <nicol@ANTEC>
2025-10-19 18:26:12 +0200

    updates

--- src/main/java/org/api/manapool/tools/ManaPoolConstants.java
+++ src/main/java/org/api/manapool/tools/ManaPoolConstants.java
@@ -3,7 +3,7 @@ package org.api.manapool.tools;
 public final class ManaPoolConstants {
 
 	public static final String BASE_URL="https://manapool.com";
-	public static final String API_VERSION="0.1";
+	public static final String API_VERSION="0.0.1";
 	public static final String API_BASE_URL="https://manapool.com/api/v1";
 	
 	private ManaPoolConstants() {
